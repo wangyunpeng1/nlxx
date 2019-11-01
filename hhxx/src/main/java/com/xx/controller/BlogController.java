38025812bef1d74b761fd495b15d1c4012f1ba7a
wangyunpeng1 <653707829@qq.com>
2019-11-01 14:09:52 +0800

    博客

--- hhxx/src/main/java/com/xx/controller/BlogController.java
+++ hhxx/src/main/java/com/xx/controller/BlogController.java
@@ -1,14 +1,15 @@
 package com.xx.controller;
 
-import com.xx.dao.BlogDao;
 import com.xx.service.BlogService;
-import com.xx.vo.Result;
-import com.xx.vo.StatusCode;
+import com.xx.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+import java.util.Map;
 
 @RestController
 @RequestMapping("pages")
@@ -26,5 +27,43 @@ public class BlogController
     public Result lookBlogId(@PathVariable("blogId") String blogId)
     {
         return new Result(true, StatusCode.OK,"查看成功",blogService.lookBlog(blogId));
+    }
+
+    /**
+     * 获取前10点赞的博客
+     * @return
+     */
+    @PostMapping("getTopTenFabulousBlogs")
+    public Result getTopTenFabulousBlogs()
+    {
+        Set<String> blogFabulous = blogService.getTopTenFabulousBlogs();
+        //id转博客
+        Map<String,String> set = blogService.getblogIdName(blogFabulous);
+        return new Result(true,StatusCode.OK,"查询成功",set);
+    }
+
+    /**
+     * 获取前10浏览的博客
+     * @return
+     */
+    @PostMapping("getTopTenVisitBlogs")
+    public Result getTopTenVisitBlogs()
+    {
+        Set<String> blogVisit = blogService.getTopTenVisitBlogs();
+        //id转博客
+        Map<String,String> set = blogService.getblogIdName(blogVisit);
+        return new Result(true,StatusCode.OK,"查询成功",set);
+    }
+    /**
+     * 获取前10收藏的博客
+     * @return
+     */
+    @PostMapping("getTopTenCollectionsBlogs")
+    public Result getTopTenCollectionsBlogs()
+    {
+        Set<String> blogCollections = blogService.getTopTenCollectionsBlogs();
+        //id转博客
+        Map<String,String> set = blogService.getblogIdName(blogCollections);
+        return new Result(true,StatusCode.OK,"查询成功",set);
     }
 }
